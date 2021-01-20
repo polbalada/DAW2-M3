29e727d1a02f445bd3f63ccde6efce2b0c6fe022
polbalada <pbalada19@jaumebalmes.net>
2021-01-20 20:52:11 +0100

    20/01/2021

--- Persones_exemple/src/main/java/com/jaumebalmes/persones_exemple/App.java
+++ Persones_exemple/src/main/java/com/jaumebalmes/persones_exemple/App.java
@@ -6,11 +6,12 @@ import java.time.Month;
 
 public class App {
     public static void main(String[] args) {
+        try{
         Persona p=new Persona("pol","balada","rigol","49284237K", LocalDate.of(2000, Month.NOVEMBER, 26));
         Persones.afegirPersona(p);
 
-        p=new Persona("FalsoPOL","balada","Rigol","49284237K", LocalDate.of(2000, Month.NOVEMBER, 26));
-        Persones.afegirPersona(p);
+//        p=new Persona("FalsoPOL","balada","Rigol","49284237K", LocalDate.of(2000, Month.NOVEMBER, 26));
+//        Persones.afegirPersona(p);
         
         p=new Persona("jhndsfkl","jsdf","kjzsdf","9213123", LocalDate.now());
         Persones.afegirPersona(p);
@@ -26,7 +27,9 @@ public class App {
         
         p=new Persona("asdsfgd","badsfs","sadsd","65128", LocalDate.now());
         Persones.afegirPersona(p);
-        
+        }catch (PersonaDuplicadaExcepcion ex){
+            System.out.println(ex.getMessage());
+        }
         System.out.println("\nOrdenat per defecte");
         Persones.mostrarTots();
         Persones.ordenarComparable();
---
+++ Persones_exemple/src/main/java/com/jaumebalmes/persones_exemple/PersonaDuplicadaExcepcion.java
@@ -0,0 +1,9 @@
+package com.jaumebalmes.persones_exemple;
+//AUTOR: Pol
+public class PersonaDuplicadaExcepcion extends Exception {
+
+    public PersonaDuplicadaExcepcion(String string) {
+        super(string);
+    }
+
+}
