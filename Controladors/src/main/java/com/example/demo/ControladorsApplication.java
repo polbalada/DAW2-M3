014e8a6a534a786ba2d934be984101a2ff631747
polbalada <pbalada19@jaumebalmes.net>
2021-03-10 20:58:00 +0100

    10/03/2021

---
+++ Controladors/src/main/java/com/example/demo/ControladorsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControladorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControladorsApplication.class, args);
+	}
+
+}
